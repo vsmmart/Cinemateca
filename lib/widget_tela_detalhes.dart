2b0af5d4c45652b3c2f8fb794dcbbb2b2609dee0
vsmmart <martins.vsm@gmail.com>
2020-06-12 18:25:23 -0300

    adicionado icone para filmes sem poster e remoção de prints de teste e comentarios

--- lib/widget_tela_detalhes.dart
+++ lib/widget_tela_detalhes.dart
@@ -10,37 +10,85 @@ class TelaDetalhes extends StatefulWidget {
 }
 
 class _TelaDetalhesState extends State<TelaDetalhes> {
-  
+  var listanotas;
+  var mapanotas;
+
   @override
   Widget build(BuildContext context) {
     Filme f = ModalRoute.of(context).settings.arguments; 
+  
     return Scaffold(
       appBar: AppBar(
         title: f == null 
         ? Text('Detalhes do Filme')
-        : Text(f.titulo, style: TextStyle(color:Colors.white),),
+        : Text('${f.titulo} - ${f.ano} ', style: TextStyle(color:Colors.white),),
         backgroundColor: Colors.deepPurple[600],
       ),
       
       body: FutureBuilder(
         future: procuraPorID(f.imdbID),
         builder: (context, snapshot){
-        if(snapshot.hasData){          
+        if(snapshot.hasData){  
             return Container(
               padding: EdgeInsets.all(20),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children:[
                    Center(child: Image.network(snapshot.data['Poster'], height: 350, width: 300,)),
                    SizedBox(height:20),
                    RichText(text: TextSpan(
                      children: [
-                       TextSpan(text:'Sinopse: ', style: TextStyle(fontWeight: FontWeight.bold) ),
-                       TextSpan(text: '${snapshot.data['Plot']}'),]
+                       TextSpan(text:'Sinopse: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                       TextSpan(text: '${snapshot.data['Plot']}', style: TextStyle(color: Colors.black)),]
+                   )),
+                    SizedBox(height:10),
+                   RichText(text: TextSpan(
+                     children: [
+                       TextSpan(text:'Gênero: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                       TextSpan(text: '${snapshot.data['Genre']}', style: TextStyle(color: Colors.black)),]
                    )),
                    SizedBox(height:10),
-                   Text('Duração: ${snapshot.data['Runtime']}'),
-                   
+                   RichText(text: TextSpan(
+                     children: [
+                       TextSpan(text:'Duração: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                       TextSpan(text: '${snapshot.data['Runtime']}', style: TextStyle(color: Colors.black)),]
+                   )),
+                   SizedBox(height:10),
+                   RichText(text: TextSpan(
+                     children: [
+                       TextSpan(text:'Diretor: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                       TextSpan(text: '${snapshot.data['Director']}', style: TextStyle(color: Colors.black)),]
+                   )),
+                  SizedBox(height:10),
+                  RichText(text: TextSpan(
+                     children: [
+                       TextSpan(text:'Atores: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                       TextSpan(text: '${snapshot.data['Actors']}', style: TextStyle(color: Colors.black)),]
+                   )),
+                  SizedBox(height:10),
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                     children: [
+                       RichText(text: TextSpan(
+                         children: [
+                           TextSpan(text:'Nota IMDB: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                           TextSpan(text: '${snapshot.data['Ratings'][0]['Value']}', style: TextStyle(color: Colors.black)),],
+                       )),
+                       
+                       RichText(text: TextSpan(
+                         children: [
+                           TextSpan(text:'Nota Rotten Tomatoes: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                           TextSpan(text: '${snapshot.data['Ratings'][1]['Value']}', style: TextStyle(color: Colors.black)),],
+                       )),
 
+                      
+                        RichText(text: TextSpan(
+                         children: [
+                           TextSpan(text:'Nota Metacritic: ', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black) ),
+                           TextSpan(text: '${snapshot.data['Metascore']}'=='N/A'?'N/A': '${snapshot.data['Metascore']}/100', style: TextStyle(color: Colors.black)),],
+                       )),
+                     ],
+                   ),
 
                 ],
                
