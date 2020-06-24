d88a1c6def337525630c98011f51ca4c566dfecc
vsmmart <martins.vsm@gmail.com>
2020-06-24 16:26:21 -0300

    Adicionado campo API em sobre

--- lib/widget_tela_sobre.dart
+++ lib/widget_tela_sobre.dart
@@ -8,6 +8,8 @@ class TelaSobre extends StatefulWidget {
 class _TelaSobreState extends State<TelaSobre> {
   TextEditingController ctrlTexto1 = TextEditingController();
   String texto1 = 'Este aplicativo foi desenvolvido como projeto para a disciplina de Programação para Dispositivos Móveis ministrada pelo professor Rodrigo Plotze. Foi também o primeiro projeto em que o desenvolvedor teve contato com POO, API, front-end, requests, Git, Github, funções assíncronas, entre outras. Foi um desafio muito grande mas motivador para o aprimoramento de programação. O Tema para o aplicativo escolhido foi "Cinema" devido a uma grande paixão do desenvolvedor por este hobby.';
+  TextEditingController ctrlTexto2 = TextEditingController();
+  String texto2 = 'A API utilizada se chama OMDB API, ela pode ser encontrada em http://www.omdbapi.com/';
   
   @override
   Widget build(BuildContext context) {
@@ -37,6 +39,27 @@ class _TelaSobreState extends State<TelaSobre> {
                   },
               ), 
             ),
+            
+            Container(
+              padding: EdgeInsets.all(20),
+              
+              margin: EdgeInsets.all(20),
+              child: TextField(                
+                  controller: ctrlTexto2 ,
+                  readOnly: true,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'API',
+                    icon: Icon(Icons.local_movies),),
+                  onTap: (){
+                    ctrlTexto2.text = texto2;
+                  },
+              ), 
+            ),
+
+
+
             FlatButton(
               onPressed: (){ Navigator.pop(context);}, 
               color: Colors.deepPurple[600],
